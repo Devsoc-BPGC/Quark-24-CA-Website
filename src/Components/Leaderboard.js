6aa40d3b0c3a4ddaacec6956cb5ffa9b141d6770
Manamrit Singh <amrit20eclectic@gmail.com>
2023-11-21 22:23:27 +0530

    The leaderboard completely works now.

--- src/Components/Leaderboard.js
+++ src/Components/Leaderboard.js
@@ -1,48 +1,127 @@
-import React from "react"
+import React, { useState, useEffect } from 'react';
 import "./Leaderboard.css"
 import Profile from "./Profiles"
-import data from "./database"
-
-export default function Leader(){
-    return(
-        <div className="Leader">
-            <header style={{position:'relative'}}>
-            <h1 style={{textAlign:'center'}}>Leaderboard</h1>
-            <a href='/' style={{marginLeft:'auto', textDecoration:'none',color:'black', fontSize:'2rem', position:'absolute', right:'2rem', top:'0rem'}}>X</a>
-            </header>
-            <div className="basic">
-                <div className="rank">
-                <b>Ranking</b>
-                </div>
-                <div className="college">
-                <b>College</b>
-                
-                </div>
-                <div className="pts">
-                <b>Points</b>
-                 <div className="idk">
-                   <div className="ptset">
-                   <p><div>{pt({data})}</div></p>
-                   
-                   </div>
-                </div>
-                </div>
-            </div>
-
-            <div className="Profiles">
-                <Profile data={data}></Profile>
-                </div>
+// import data from "./database";
+// import {length} from "./database";
 
 
+
+
+
+
+export default function Leader() {
+
+  
+
+   
+  const [tdata, setTdata] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://sheet.best/api/sheets/1559505e-569c-4527-ad0f-0d0c7ce65c37?_raw=1");
+        const data = await response.json();
+       
+        const sortedData = data.sort((a, b) => b.Points - a.Points);
+        setTdata(sortedData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+  
+    fetchData(); 
+  }, []); 
+  
+
+  const tlength = tdata.length;
+  
+    
+
+     
+
+    
+   
+
+
+
+
+  return (
+    <div className="Leader">
+      <header style={{ position: 'relative' }}>
+        <h1 style={{ textAlign: 'center' }}>Leaderboard</h1>
+        <a href='/' style={{ marginLeft: 'auto', textDecoration: 'none', color: 'black', fontSize: '2rem', position: 'absolute', right: '2rem', top: '0rem' }}>X</a>
+      </header>
+      <div className="basic">
+        <div className="rank">
+          <b>Ranking</b>
+          <p><div className="move1">{rank(tlength)}</div></p>
         </div>
-    )
+        <div className="college">
+          <div className="end">
+            <b>College</b>
+          </div>
+          <p><div className="move2">{namae( tdata )}</div></p>
+        </div>
+        <div className="pts">
+          <b>Points</b>
+          <div className="idk">
+            <div className="ptset">
+              <p><div>{pt(tdata )}</div></p>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* <div className="Profiles">
+                <Profile data={data}></Profile>
+                </div> */}
+
+
+    </div>
+  )
 }
 
-function pt({data}){
-  return(
-    data.map((value,index)=>(
-    <div className="finally">
-        <p>{value.points}</p>
-    </div>
-  )))
+function pt( data ) {
+  return (
+    data.map((value, index) => (
+      <div className="finally">
+        <p>{value.Points}</p>
+      </div>
+    )))
 };
+
+function namae( data ) {
+  return (
+    data.map((value, index) => (
+      <div className="finally2">
+        <p>{value.College}</p>
+      </div>
+    )))
+};
+
+// function rank(length) {
+//   const rankno=[]
+//   for(let i=1; i<=length; i++){
+//    rankno.push(<li key={i}>Item {i + 1}</li>);
+//   }
+//   return rankno;
+// };
+
+
+  const rank = (length) => {
+    const rankno = [];
+    for (let i = 1; i <= length; i++) {
+      rankno.push(<li key={i}>{i}</li>);
+    }
+  
+    return (
+      rankno.map((value,index)=>(
+        <div className="betterwork">
+          <p>{value}</p>
+        </div>
+      ))
+    )
+    }
+
+  
--- src/Components/database.js
+++ src/Components/database.js
@@ -1,57 +1,66 @@
-const data=[
-    { 
-      rank: 1 ,
-      name:"BITS Pilani KK Birla Goa Campus",
-      points:100
-    },
-  
-    {
-      rank: 2,
-      name: "IIT Bombay",
-      points: 99
-    },
-    
-    {
-      rank:3,
-      name:"BITS Pilani Pilani Campus",
-      points: 95
-    },
-    
-    {
-      rank:4,
-      name:"VIT",
-      points: 90
-    },
-    
-    {
-      rank:5,
-      name:"Thapar",
-      points:80
-    },
-    {
-        rank:6,
-        name:"Local Uni",
-        points: 0
-      },
-      {
-        rank:7,
-        name:"Local Uni",
-        points: 0
-      },
-      {
-        rank:8,
-        name:"Local Uni",
-        points: 0
-      },
+const data = [
+  {
+    rank: 1,
+    name: "BITS Pilani KK Birla Goa Campus",
+    points: 100
+  },
 
-      {
-        rank: 9,
-        name:"Local Uni",
-        points: 0
-      },
+  {
+    rank: 2,
+    name: "IIT Bombay",
+    points: 99
+  },
 
-     
-    
-  ];
-  
-  export default data;
+  {
+    rank: 3,
+    name: "BITS Pilani Pilani Campus",
+    points: 95
+  },
+
+  {
+    rank: 4,
+    name: "VIT",
+    points: 90
+  },
+
+  {
+    rank: 5,
+    name: "Thapar",
+    points: 80
+  },
+  {
+    rank: 6,
+    name: "Local Uni",
+    points: 0
+  },
+  {
+    rank: 7,
+    name: "Local Uni",
+    points: 0
+  },
+  {
+    rank: 8,
+    name: "Local Uni",
+    points: 0
+  },
+
+  {
+    rank: 9,
+    name: "Local Uni",
+    points: 0
+  },
+
+
+
+];
+
+data.sort((a, b) => b.points - a.points);
+
+export const length = data.length;
+
+const box=length;
+
+console.log(box)
+console.log(length)
+
+export default data;
